9228a87edc9c8fb21c54b717a1c545e3769c9c57
DChelo <lucumichelo2003gmail.com>
2023-05-10 23:34:25 -0500

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      this.router.navigateByUrl('reservation');
+      this.router.navigateByUrl('splash');
     });
   }
 }
