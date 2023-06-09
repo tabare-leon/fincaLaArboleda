a7c2e9467c185445d4ee589031a0f4d403cba14c
Nicolas tabares <nleon9791@gmail.com>
2023-06-08 21:33:28 -0500

    ♻️ add  property API to environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  API: "https://api-cabins-production.up.railway.app/api-cabins"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API: "https://api-cabins-production.up.railway.app/api-cabins"
 };
 
 /*
